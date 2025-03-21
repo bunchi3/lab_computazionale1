379f989fcd7ab436bf4699261d806c3e635bb150
bunchi3 <s.franceschina@campus.unimib.it>
2025-03-21 20:35:37 +0100

    Esercizio 4.2: prima versione, manca il determinante della matrice Aggiunte funzioni alla libreria per le matrici: prodotto esterno e decomposizione di una matrice

--- librerie/linear_systems.jl
+++ librerie/linear_systems.jl
@@ -1,3 +1,6 @@
+#Import section
+using LinearAlgebra
+
 #This function operates a forward substitution for lower diagonal matrices (vector of vectors and matrices)
 function fw_sub(matrix::Vector{Vector{Int64}}, b)
     n = length(matrix)
@@ -192,3 +195,34 @@ function bw_sub(matrix::Matrix{Float64}, b)
     return solution
 end
 
+#Funzione per il prodotto esterno. Restituisce una matrice a partire da due vettori
+function outer_product(v, w)
+    if length(v) != length(w)
+        throw(DomainError(v, "Dimension mismatch between the two arrays"))
+    end
+    n = length(v)
+    matrix = zeros(n, n)
+    for i in 1:n
+        for j in 1:n
+            matrix[i, j] = v[i]*w[j]
+        end
+    end
+    return matrix
+end
+
+#Esegue la decomposizione di una matrice in due matrici, una triangolare inferiore e l'altra triangolare superiore
+#NON INCLUDE ROW-PIVOTING
+function LU_dec(A)
+    n = size(A, 1)
+    L = diagm(ones(n))
+    U = zeros(n, n)
+    #B matrice da usare per i calcoli intermedi
+    B = A
+    #i indice dell'iterazione. Faccio in modo di escludere gli zeri
+    for i in 1:n
+        U[i, i:n] = B[i, i:n]
+        L[i:n, i] = B[i:n, i]/U[i,i]
+        B -= outer_product(L[:,i], U[i,:])       
+    end
+    return L, U
+end
