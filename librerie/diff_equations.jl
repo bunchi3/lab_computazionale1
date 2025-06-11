69e2dae13396bc796c77263bd7c1af6d3ea5a5ef
bunchi3 <s.franceschina@campus.unimib.it>
2025-06-11 20:23:45 +0200

    Tentativo di implementazione del metodo adattivo per equazioni differenziali Messa in relazione di 5_1_2 e 5_1_1

--- librerie/diff_equations.jl
+++ librerie/diff_equations.jl
@@ -1,7 +1,9 @@
 #File's index:
 #1. IMPORT SECTION
-#2. EULER'S METHOD
-#3. RUNGE-KUTTA METHOD
+#2. STEP SIZE FINDING
+#3. SOLUTION LONG STEADY STATE
+#4. EULER'S METHOD
+#5. RUNGE-KUTTA METHOD
 #-------------------------------------------------------------------------------------------------------------------------------
 #IMPORT SECTION
 #-------------------------------------------------------------------------------------------------------------------------------
@@ -44,6 +46,83 @@ function h_search(ODE_func1::Function, ODE_func2::Function, f::Vector, u0::Vecto
         throw(DomainError(n, "h_search says: the two methods are not stable."))
     end
     return abs((b - a)/(2^(iter)*n))
+end
+
+#This function computes the next solution point given h
+function BS2(f::Vector, ui::Vector, ti::Number, h::Float64)
+    m = length(f)
+    if m != length(ui)
+        throw(DomainError(ui, "euler_ode says: the two parameters must be of the same length."))
+    end
+
+    u = zeros(m)
+
+    #j will be used as index for the diff_equations  
+    #Defining a vector containing all the entries for the known function      
+    v = [ui[j] for j in 1:1:m]
+    k1 = [h*f[j](ti, v...) for j in 1:1:m]
+    v_stage1 = [ui[j] + 0.5*k1[j] for j in 1:1:m]
+    k2 = [h*f[j](ti + 0.5*h, v_stage1...) for j in 1:1:m]  
+    v_stage2 = [ui[j] + (3.0/4.0)*k2[j] for j in 1:1:m]
+    k3 = [h*f[j](ti + (3.0/4.0)*h, v_stage2...) for j in 1:1:m]
+    v_stage3 = [ui[j] + (2.0/9.0)*k1[j] + (1.0/3.0)*k2[j] + (4.0/9.0)*k3[j] for j in 1:1:m]
+    k4 = [h*f[j](ti + h, v_stage3...) for j in 1:1:m] 
+    for j in 1:1:m    
+        u[j] = ui[j] + (7/24)*k1[j] + (1/4)*k2[j] + (1/3)*k3[j] + (1/8)*k4[j]
+    end 
+
+    #u contains m vectors, each of length n+1
+    return u
+end
+
+#This function computes the next solution point given h
+function BS3(f::Vector, ui::Vector, ti::Number, h::Float64)
+    m = length(f)
+    if m != length(ui)
+        throw(DomainError(ui, "euler_ode says: the two parameters must be of the same length."))
+    end
+
+    u = zeros(m)   
+
+    #j will be used as index for the diff_equations
+    #Defining a vector containing all the entries for the known function      
+    v = [ui[j] for j in 1:1:m]
+    k1 = [h*f[j](ti, v...) for j in 1:1:m]
+    v_stage1 = [ui[j] + 0.5*k1[j] for j in 1:1:m]  
+    k2 = [h*f[j](ti + 0.5*h, v_stage1...) for j in 1:1:m]   
+    v_stage2 = [ui[j] + (3.0/4.0)*k2[j] for j in 1:1:m]
+    k3 = [h*f[j](ti + (3.0/4.0)*h, v_stage2...) for j in 1:1:m]
+    v_stage3 = [ui[j] + (2.0/9.0)*k1[j] + (1.0/3.0)*k2[j] + (4.0/9.0)*k3[j] for j in 1:1:m]
+    #k4 = [h*f[j](ti + h, v_stage3...) for j in 1:1:m]
+    for j in 1:1:m         
+        u[j] = ui[j] + (2.0/9.0)*k1[j] + (1.0/3.0)*k2[j] + (4.0/9.0)*k3[j]
+    end
+
+    #u contains m vectors, each of length n+1
+    return u
+end
+
+function adaptive_bs23(f::Vector, ui::Vector, ti::Number, δ::Float64)
+    exit = false
+    h = δ^(1.0/3.0) / 2.0
+    q = 1.0
+    H = q*h
+    u_num1::Vector = []
+    u_num2::Vector = []
+    while exit == false
+        H = q*h
+        #u_num is u_{i+1}
+        u_num1 = BS2(f, ui, ti, H)    #computing the next point vector of the solution for every equation
+        u_num2 = BS3(f, ui, ti, H)    #computing the next point vector of the solution for every equation
+        ϵi = δ*(1 + maximum(abs.(ui)))
+        Ei = maximum(abs.(u_num1 - u_num2))
+        if Ei < ϵi
+            exit = true
+        end
+        q = 0.8 * (ϵi/Ei)^(1.0/3.0)
+        q = min(q, 4)
+    end
+    return u_num2, H
 end
 #-------------------------------------------------------------------------------------------------------------------------------
 #SOLUTION LONG STEADY STATE
@@ -177,6 +256,112 @@ function RK4(f::Vector, u0::Vector, n::Int, a::Number, b::Number)
 
     #u contains m vectors, each of length n+1
     return u
+end
+
+#= function BS2(f::Vector, u0::Vector, n::Int, a::Number, b::Number)
+    m = length(f)
+    if m != length(u0)
+        throw(DomainError(u0, "euler_ode says: the two parameters must be of the same length."))
+    end
+
+    h = (b-a)/n
+    u = [Vector{Number}(undef, n+1) for _ in 1:1:m]     #n+1 because we need to include the initial point
+
+    #j will be used as index for the diff_equations
+    #i will be used as index for the points of the single equation
+    #for loop for the initial point
+    for j in 1:1:m
+        u[j][1] = u0[j]        
+    end
+
+    #for loop for the rest of the points, starting from the first one, excluding the known value (u0)
+    for i in 1:1:n
+        ti = a + (i-1)*h  
+        #Defining a vector containing all the entries for the known function      
+        v = [u[j][i] for j in 1:1:m]
+        k1 = [h*f[j](ti, v...) for j in 1:1:m]
+        v_stage1 = [u[j][i] + 0.5*k1[j] for j in 1:1:m]  
+        k2 = [h*f[j](ti + 0.5*h, v_stage1...) for j in 1:1:m]   
+        v_stage2 = [u[j][i] + (3.0/4.0)*k2[j] for j in 1:1:m]
+        k3 = [h*f[j](ti + (3.0/4.0)*h, v_stage2...) for j in 1:1:m]
+        v_stage3 = [u[j][i] + (2.0/9.0)*k1[j] + (1.0/3.0)*k2[j] + (4.0/9.0)*k3[j] for j in 1:1:m]
+        k4 = [h*f[j](ti + h, v_stage3...) for j in 1:1:m]
+        for j in 1:1:m         
+            u[j][i+1] = u[j][i] + ((7/24)*k1[j] + (1/4)*k2[j] + (1/3)*k3[j] + (1/8)*k4[j])
+        end
+    end
+
+    #u contains m vectors, each of length n+1
+    return u
+end
+
+function BS3(f::Vector, u0::Vector, n::Int, a::Number, b::Number)
+    m = length(f)
+    if m != length(u0)
+        throw(DomainError(u0, "euler_ode says: the two parameters must be of the same length."))
+    end
+
+    h = (b-a)/n
+    u = [Vector{Number}(undef, n+1) for _ in 1:1:m]     #n+1 because we need to include the initial point
+
+    #j will be used as index for the diff_equations
+    #i will be used as index for the points of the single equation
+    #for loop for the initial point
+    for j in 1:1:m
+        u[j][1] = u0[j]        
+    end
+
+    #for loop for the rest of the points, starting from the first one, excluding the known value (u0)
+    for i in 1:1:n
+        ti = a + (i-1)*h  
+        #Defining a vector containing all the entries for the known function      
+        v = [u[j][i] for j in 1:1:m]
+        k1 = [h*f[j](ti, v...) for j in 1:1:m]
+        v_stage1 = [u[j][i] + 0.5*k1[j] for j in 1:1:m]  
+        k2 = [h*f[j](ti + 0.5*h, v_stage1...) for j in 1:1:m]   
+        v_stage2 = [u[j][i] + (3.0/4.0)*k2[j] for j in 1:1:m]
+        k3 = [h*f[j](ti + (3.0/4.0)*h, v_stage2...) for j in 1:1:m]
+        v_stage3 = [u[j][i] + (2.0/9.0)*k1[j] + (1.0/3.0)*k2[j] + (4.0/9.0)*k3[j] for j in 1:1:m]
+        #k4 = [h*f[j](ti + h, v_stage3...) for j in 1:1:m]
+        #first same as last
+        for j in 1:1:m         
+            u[j][i+1] = v_stage3[j]
+        end
+    end
+
+    #u contains m vectors, each of length n+1
+    return u
+end =#
+#-------------------------------------------------------------------------------------------------------------------------------
+function solution_adapt_bs23(f::Vector, u0::Vector, a::Number, b::Number, δ::Float64)
+    m = length(f)
+    u = [[] for _ in 1:1:m]     #n+1 because we need to include the initial point
+    t = [a]
+    h = []
+    #i index of the solution Number
+    #j index of the equations
+    for j in 1:1:m
+        push!(u[j],  u0[j])        
+    end
+
+    #ui_1 is u_{i+1} and hi1 is the time step to reach t_{i+1}
+    while t[end] < b
+        ui_1, hi_1 = adaptive_bs23(f, [u[j][end] for j in 1:1:m], t[end], δ) 
+        push!(h, hi_1) 
+        ti = t[end]
+        #Interrupting the routine if h doesn't make the time grow.
+        if ti == ti + hi_1
+            return t, u
+        end
+        for j in 1:1:m
+            push!(u[j], ui_1[j])            
+        end  
+        push!(t, ti + hi_1)
+    end
+        
+    return t, u, h
+    
+    
 end
 #-------------------------------------------------------------------------------------------------------------------------------
 println("diff_equations.jl loaded correctly")
